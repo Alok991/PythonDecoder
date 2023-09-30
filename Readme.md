27f8b4a036f71422b288744d494d2006bcaa8160
Alok Dixit <alok@e4capital.com>
2023-10-01 00:04:40 +0800

    Working

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Steps to run
+
+1. pip install -r requirements.txt
+2. flatc --python client.fbs
+3. python fb_decoder.py <PATH_TO_FILE_GENERATE_BY_CPP>
--- fb_decoder.py
+++ fb_decoder.py
@@ -9,22 +9,35 @@ if len(sys.argv) < 2:
 
 fb_bin_file = sys.argv[1]
 
-with open(fb_bin_file, "rb") as bin_file:
-    flatbuffer_data = bin_file.read()
 
-    # import ipdb;ipdb.set_trace()
+def deserialize_and_print(flatbuffer_data):
     client = Client.Client.GetRootAs(flatbuffer_data, 0)
     if client.ClientTypeType() == ClientType.ClientType().Person:
         p = Person.Person()
         p.Init(client.ClientType().Bytes, client.ClientType().Pos)
         gender = "Male" if p.Gender()==0 else "Female"
         print(f"{{{p.Name().decode('utf-8')}, {p.Age()}, {p.Weight()}, {gender}}}")
-
-
-    client = Client.Client.GetRootAs(flatbuffer_data, p.PersonLength())
     if client.ClientTypeType() == ClientType.ClientType().Group:
         g = Group.Group()
         g.Init(client.ClientType().Bytes, client.ClientType().Pos)
-        print(g.GroupName())
-    
+        
+        all_names = []
+        for i in range(0, g.NamesListLength()):
+            all_names.append(g.NamesList(i).decode('utf-8'))
 
+        print(f"{{{ g.GroupName().decode('utf-8')}, {g.AverageAge()}, {g.AverageWeight()}, {{{  ','.join(all_names)  }}}}}")
+    
+#{FightClub, 24.5, 66, {Ram, Shayam, Raghuveer} }
+
+with open(fb_bin_file, "rb") as bin_file:
+    buffer = bin_file.read()
+    idx = 0
+    total_sz = len(buffer)
+    
+    while idx < total_sz:
+        # read size of buffer
+        size_of_buffer = int.from_bytes(buffer[idx:idx+8], "little")
+        idx += 8
+        flatbuffer_data = buffer[idx:idx+size_of_buffer]
+        idx=idx+size_of_buffer
+        deserialize_and_print(flatbuffer_data)
